a187f9ac08b123904dde82694657b80516e933cf
bbesecke <>
2022-08-26 15:37:35 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@UnwieldyEmbellishedInitializationfile/CSCI130 operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "2 + 5 = "<< 2 + 5 << endl;
+  cout << " 2 / 5 = " << 2.0 / 5 << endl;
+  cout << " 7.3 * 2.4 = " << 7.0 * 2.0 << endl;
+  cout << " 10 % 4 = " << 10 % 4 << endl;
+  cout << " - 10 % 4 = " << - 10 % 4 << endl;
+  cout << " 2.37E-5 * 5.23E-8 = " << 2.37E-5 * 5.23E-8 << endl;
+  cout << "3.5/2+1="<< 3.5/2+1 << endl;
+  double a = 3.5;
+  int b = 2;
+  int c = 1;
+  int d;
+  double e;
+  d = a / b + c;
+  e = a / b + c;
+}
